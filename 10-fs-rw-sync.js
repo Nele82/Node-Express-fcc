cd3cd1e2fcf89ee6448f1eabd9d54570b0ce9159
Nebojsa <nebojsap82@gmail.com>
2024-01-06 00:17:51 +0100

    README.md file update

--- 10-fs-rw-sync.js
+++ 10-fs-rw-sync.js
@@ -1,15 +1,16 @@
 // FS MODULE (READ & WRITE - SYNC)
 
 const fs = require('fs')
-
+console.log('start')
 const first = fs.readFileSync('./content/first.txt', 'utf8')
 const second = fs.readFileSync('./content/subfolder/second.txt', 'utf8')
-
-console.log(first, second)
 
 fs.writeFileSync(
     './content/result-sync.txt', 
     `Here is the result: ${first} ${second}`,
     {flag: 'a'} // Flag 'a' stands for append meaning that, when the 'a' flag is set, whatever is set to be written in the designated
     // file would be added in addition to what is already in the file. Otherwise, the content of the file would just be owerwritten
-    )
+)
+
+console.log('done')
+console.log('starting the next task')
