f7e2d39382c4c075ad0d5c88e8351344e93fb182
Nebojsa <nebojsap82@gmail.com>
2024-01-19 11:19:06 +0100

    Express.js + README.md update (part: 4)

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,10 +2,13 @@ const http = require('http')
 const {readFileSync} = require('fs')
 
 // get all files
-const homePage = readFileSync('./index.html')
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
-const server = http.createServer((req, res)=>{
-    // console.log(req.url)
+const server = http.createServer((req, res) => {
+    console.log(req.url)
 
     // Home page
     if(req.url == '/') {
@@ -21,7 +24,31 @@ const server = http.createServer((req, res)=>{
             'content-type': 'text/html'
         })
         res.write('<h1>About page</h1>')
-        res.end()
+        res.end();
+    
+    // Styles
+    } else if (req.url == '/styles.css') {
+        res.writeHead(200, {
+            'content-type': 'text/css'
+        })
+        res.write(homeStyles)
+        res.end();
+    
+    // Logo
+    } else if (req.url == '/logo.svg') {
+        res.writeHead(200, {
+            'content-type': 'image/svg+xml'
+        })
+        res.write(homeImage)
+        res.end();
+    
+    // Browser logic
+    } else if (req.url == '/browser-app.js') {
+        res.writeHead(200, {
+            'content-type': 'text/javascript'
+        })
+        res.write(homeLogic)
+        res.end();
     
     // 404
     } else {
@@ -29,8 +56,8 @@ const server = http.createServer((req, res)=>{
             'content-type': 'text/html'
         })
         res.write('<h1>Page not found!</h1>')
-        res.end()
+        res.end();
     }   
 })
 
-server.listen(5000) 
+server.listen(5000)
