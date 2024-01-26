79384faa4040d46af72c521930dc6dfd1e4c70bf
Nebojsa <nebojsap82@gmail.com>
2024-01-26 09:02:16 +0100

    Express.js - Route params

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,20 +1,42 @@
 const express = require('express')
-const path = require('path')
 const app = express()
+const {products} = require('./data')
+const {people} = require('./data')
 
-app.use(express.static('./public'))
+app.get('/', (req, res)=>{
+    res.send('<h1>Home page</h1><a href="/api/products">Products</a>')
+})
 
-// app.get('/', (req, res) => {
-//    res.sendFile(path.resolve(__dirname, 'navbar-app/index.html'))
-//     Adding to static assets
+app.get('/api/products', (req, res)=>{
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product
+        return {id, name, image}
+    })
+    res.json(newProducts)
+})
 
+app.get('/api/products', (req, res)=>{
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product
+        return {id, name, image}
+    })
+    res.json(newProducts)
+})
 
-// })
+app.get('/api/products/:productID', (req, res)=>{
+    const {productID} = req.params
+    const singleProduct = products.find(product => product.id === Number(productID))
+    if (!singleProduct) {
+        res.status(404).send('Product does not exist!')
+    }
+    res.send(singleProduct)
+})
 
-app.all('*', (req, res) => {
-    res.status(404).send('Requested resource has not been found.')
+app.get('/api/products/:productID/reviews/:reviewID', (req, res)=>{
+
+    res.send('Hello World!')
 })
 
 app.listen(5000, ()=>{
-    console.log('Server listening on port 5000')
+    console.log('Server is listening on port 5000')
 })
