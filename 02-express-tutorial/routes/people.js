7a34da09c4490a723aa34e912f0f2ec0c43f80e1
Nebojsa <nebojsap82@gmail.com>
2024-01-31 19:42:48 +0100

    Express.js - Controllers

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -1,57 +1,23 @@
 const express = require('express')
 const router = express.Router()
 let { people } = require('../data')
+const {
+    getPeople,
+    createPerson,
+    createPersonPostman,
+    updatePerson,
+    deletePerson
+} = require('../controllers/people')
 
-router.get('/', (req, res)=>{
-    res.status(200).json({ success: true, data: people})
-})
+router.get('/', getPeople)
 
-router.post('/', (req, res)=>{
-    const { name } = req.body
-    if(!name) {
-        return res.status(400).json({success: false, msg: 'Please provide the name value!'})
-    }
-    res.status(201).send({success: true, person: name})
-})
+router.post('/', createPerson)
 
-router.post('/postman', (req, res)=>{
-    const { name } = req.body
-    if(!name) {
-        return res.status(400).json({success: false, msg: 'Please provide the name value!'})
-    }
-    res.status(201).send({success: true, data: [...people, name]})
-})
+router.post('/postman', createPersonPostman)
 
-router.put('/:id', (req, res)=>{
-    const {id} = req.params
-    const {name} = req.body
-    
-    const person = people.find((person)=> person.id === Number(id))
+router.put('/:id', updatePerson)
 
-    if(!person) {
-        return res
-            .status(404)
-            .json({success: false, msg: `no person with id ${id}`})
-    }
-    const newPeople = people.map((person)=>{
-        if(person.id === Number(id)) {
-            person.name = name
-        }
-        return person
-    })
-    res.status(200).json({success: true, data: newPeople})
-})
-
-router.delete('/:id', (req, res)=>{
-    const {id} = req.params    
-    const person = people.find((person)=> person.id === Number(id))
-    if(!person) {
-        return res
-            .status(404)
-            .json({success: false, msg: `no person with id ${id}`})
-    }
-    const newPeople = people.filter(el => el.id != Number(id))
-    res.status(200).json({success: true, data: newPeople})
-})
+router.delete('/:id', deletePerson)
 
 module.exports = router
+
